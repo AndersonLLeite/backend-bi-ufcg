969307a9af4aaf77cda0148ea707096e2f476a4b
Anderson de Lima Leite <anderson.leite@ccc.ufcg.edu.br>
2024-12-17 23:06:31 -0300

    testando json

--- src/main/java/com/ufcg/bi/controllers/DataController.java
+++ src/main/java/com/ufcg/bi/controllers/DataController.java
@@ -54,75 +54,120 @@ public class DataController {
     }
 
     @GetMapping("/courses_json")
-    public ResponseEntity<?> getCoursesJson() {
-        List<Map<String, Object>> response = new ArrayList<>();
+public ResponseEntity<?> getCoursesJson() {
+    List<Map<String, Object>> response = new ArrayList<>();
 
-        // Curso 1
-        Map<String, Object> course1 = new HashMap<>();
-        course1.put("codigo_do_curso", 101);
-        course1.put("descricao", "Curso de Engenharia de Computação");
-        course1.put("status", "Ativo");
-        course1.put("codigo_do_setor", 15);
-        course1.put("nome_do_setor", "Centro de Engenharia Elétrica e Informática");
-        course1.put("grau_do_curso", "Bacharelado");
-        course1.put("campus", 1);
-        course1.put("nome_do_campus", "Campina Grande");
+    // Curso 1
+    Map<String, Object> course1 = new HashMap<>();
+    course1.put("codigo_do_curso", 101);
+    course1.put("descricao", "Curso de Engenharia de Computação");
+    course1.put("status", "Ativo");
+    course1.put("codigo_do_setor", 15);
+    course1.put("nome_do_setor", "Centro de Engenharia Elétrica e Informática");
+    course1.put("grau_do_curso", "Bacharelado");
+    course1.put("campus", 1);
+    course1.put("nome_do_campus", "Campina Grande");
 
-        List<Map<String, Object>> periods1 = new ArrayList<>();
-        Map<String, Object> period1 = new HashMap<>();
-        period1.put("periodo", "2020.1");
-        period1.put("gender_distribution", Map.of("male", 60.0, "female", 40.0));
-        period1.put("age_distribution", Map.of("18-20", 50.0, "21-25", 40.0, "26+", 10.0));
-        period1.put("affirmative_policy_distribution", Map.of("yes", 30.0, "no", 70.0));
-        periods1.add(period1);
+    List<Map<String, Object>> periods1 = new ArrayList<>();
+    Map<String, Object> period1 = new HashMap<>();
+    period1.put("periodo", "2020.1");
+    period1.put("gender_distribution", Map.of("male", 60.0, "female", 40.0));
+    period1.put("age_distribution", Map.of("18-20", 50.0, "21-25", 40.0, "26+", 10.0));
+    period1.put("affirmative_policy_distribution", Map.of("yes", 30.0, "no", 70.0));
+    periods1.add(period1);
 
-        course1.put("periodos", periods1);
-        response.add(course1);
+    course1.put("periodos", periods1);
+    response.add(course1);
 
-        // Curso 2
-        Map<String, Object> course2 = new HashMap<>();
-        course2.put("codigo_do_curso", 102);
-        course2.put("descricao", "Curso de Matemática");
-        course2.put("status", "Inativo");
-        course2.put("codigo_do_setor", 16);
-        course2.put("nome_do_setor", "Centro de Ciências Exatas e da Natureza");
-        course2.put("grau_do_curso", "Licenciatura");
-        course2.put("campus", 2);
-        course2.put("nome_do_campus", "Patos");
+    // Curso 2
+    Map<String, Object> course2 = new HashMap<>();
+    course2.put("codigo_do_curso", 102);
+    course2.put("descricao", "Curso de Matemática");
+    course2.put("status", "Inativo");
+    course2.put("codigo_do_setor", 16);
+    course2.put("nome_do_setor", "Centro de Ciências Exatas e da Natureza");
+    course2.put("grau_do_curso", "Licenciatura");
+    course2.put("campus", 2);
+    course2.put("nome_do_campus", "Patos");
 
-        List<Map<String, Object>> periods2 = new ArrayList<>();
-        Map<String, Object> period2 = new HashMap<>();
-        period2.put("periodo", "2019.2");
-        period2.put("gender_distribution", Map.of("male", 40.0, "female", 60.0));
-        period2.put("age_distribution", Map.of("18-20", 30.0, "21-25", 50.0, "26+", 20.0));
-        period2.put("affirmative_policy_distribution", Map.of("yes", 40.0, "no", 60.0));
-        periods2.add(period2);
+    List<Map<String, Object>> periods2 = new ArrayList<>();
+    Map<String, Object> period2 = new HashMap<>();
+    period2.put("periodo", "2019.2");
+    period2.put("gender_distribution", Map.of("male", 40.0, "female", 60.0));
+    period2.put("age_distribution", Map.of("18-20", 30.0, "21-25", 50.0, "26+", 20.0));
+    period2.put("affirmative_policy_distribution", Map.of("yes", 40.0, "no", 60.0));
+    periods2.add(period2);
 
-        course2.put("periodos", periods2);
-        response.add(course2);
+    course2.put("periodos", periods2);
+    response.add(course2);
 
-        // Curso 3
-        Map<String, Object> course3 = new HashMap<>();
-        course3.put("codigo_do_curso", 103);
-        course3.put("descricao", "Curso de Direito");
-        course3.put("status", "Ativo");
-        course3.put("codigo_do_setor", 17);
-        course3.put("nome_do_setor", "Centro de Ciências Jurídicas");
-        course3.put("grau_do_curso", "Bacharelado");
-        course3.put("campus", 3);
-        course3.put("nome_do_campus", "Sousa");
+    // Curso 3
+    Map<String, Object> course3 = new HashMap<>();
+    course3.put("codigo_do_curso", 103);
+    course3.put("descricao", "Curso de Direito");
+    course3.put("status", "Ativo");
+    course3.put("codigo_do_setor", 17);
+    course3.put("nome_do_setor", "Centro de Ciências Jurídicas");
+    course3.put("grau_do_curso", "Bacharelado");
+    course3.put("campus", 3);
+    course3.put("nome_do_campus", "Sousa");
 
-        List<Map<String, Object>> periods3 = new ArrayList<>();
-        Map<String, Object> period3 = new HashMap<>();
-        period3.put("periodo", "2021.1");
-        period3.put("gender_distribution", Map.of("male", 55.0, "female", 45.0));
-        period3.put("age_distribution", Map.of("18-20", 40.0, "21-25", 45.0, "26+", 15.0));
-        period3.put("affirmative_policy_distribution", Map.of("yes", 35.0, "no", 65.0));
-        periods3.add(period3);
+    List<Map<String, Object>> periods3 = new ArrayList<>();
+    Map<String, Object> period3 = new HashMap<>();
+    period3.put("periodo", "2021.1");
+    period3.put("gender_distribution", Map.of("male", 55.0, "female", 45.0));
+    period3.put("age_distribution", Map.of("18-20", 40.0, "21-25", 45.0, "26+", 15.0));
+    period3.put("affirmative_policy_distribution", Map.of("yes", 35.0, "no", 65.0));
+    periods3.add(period3);
 
-        course3.put("periodos", periods3);
-        response.add(course3);
+    course3.put("periodos", periods3);
+    response.add(course3);
 
-        return ResponseEntity.ok(response);
-    }
+    // Curso 4
+    Map<String, Object> course4 = new HashMap<>();
+    course4.put("codigo_do_curso", 104);
+    course4.put("descricao", "Curso de Administração");
+    course4.put("status", "Ativo");
+    course4.put("codigo_do_setor", 18);
+    course4.put("nome_do_setor", "Centro de Ciências Sociais Aplicadas");
+    course4.put("grau_do_curso", "Bacharelado");
+    course4.put("campus", 1);
+    course4.put("nome_do_campus", "Campina Grande");
+
+    List<Map<String, Object>> periods4 = new ArrayList<>();
+    Map<String, Object> period4 = new HashMap<>();
+    period4.put("periodo", "2022.1");
+    period4.put("gender_distribution", Map.of("male", 50.0, "female", 50.0));
+    period4.put("age_distribution", Map.of("18-20", 60.0, "21-25", 30.0, "26+", 10.0));
+    period4.put("affirmative_policy_distribution", Map.of("yes", 25.0, "no", 75.0));
+    periods4.add(period4);
+
+    course4.put("periodos", periods4);
+    response.add(course4);
+
+    // Curso 5
+    Map<String, Object> course5 = new HashMap<>();
+    course5.put("codigo_do_curso", 105);
+    course5.put("descricao", "Curso de Medicina");
+    course5.put("status", "Inativo");
+    course5.put("codigo_do_setor", 19);
+    course5.put("nome_do_setor", "Centro de Ciências Médicas");
+    course5.put("grau_do_curso", "Bacharelado");
+    course5.put("campus", 4);
+    course5.put("nome_do_campus", "Cajazeiras");
+
+    List<Map<String, Object>> periods5 = new ArrayList<>();
+    Map<String, Object> period5 = new HashMap<>();
+    period5.put("periodo", "2023.2");
+    period5.put("gender_distribution", Map.of("male", 45.0, "female", 55.0));
+    period5.put("age_distribution", Map.of("18-20", 35.0, "21-25", 50.0, "26+", 15.0));
+    period5.put("affirmative_policy_distribution", Map.of("yes", 20.0, "no", 80.0));
+    periods5.add(period5);
+
+    course5.put("periodos", periods5);
+    response.add(course5);
+
+    return ResponseEntity.ok(response);
+}
+
 }
