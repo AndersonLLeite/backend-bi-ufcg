da8dfc0106e69a99f2b94cde809e09bb31e30a2c
Anderson Leite <anderson.tom.leite@gmail.com>
2024-12-20 17:00:08 -0300

    Mudando lógica

---
+++ src/main/java/com/ufcg/bi/models/Metrica.java
@@ -0,0 +1,19 @@
+package com.ufcg.bi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "terms")
+public class Metric {
+
+
+    
+}
