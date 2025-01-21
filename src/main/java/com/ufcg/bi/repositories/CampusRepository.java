5473d5e9f1c46b593c809fe9bfaa5b0cf7e43688
Anderson Leite <anderson.tom.leite@gmail.com>
2025-01-21 13:51:51 -0300

    Refactor: Excluindo classes e metodos obsoletos

--- src/main/java/com/ufcg/bi/repositories/CampusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ufcg.bi.repositories;
-
-import com.ufcg.bi.models.Campus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CampusRepository extends JpaRepository<Campus, Long> {
-}
--- src/main/java/com/ufcg/bi/repositories/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ufcg.bi.repositories;
-
-import com.ufcg.bi.models.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-}
